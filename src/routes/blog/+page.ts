5d98584b75aaa5ed17439070aa259e676aa8f2a7
a96lex <a96lex@gmail.com>
2023-02-20 18:20:45 +0100

    delete logs, update import

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -3,19 +3,17 @@ import { error } from "@sveltejs/kit";
 export const load = async () => {
 	try {
 		const allPostFiles = import.meta.glob("../../lib/blogEntries/*.md");
+		console.log(allPostFiles);
+
 		const iterablePostFiles = Object.keys(allPostFiles);
 
 		let posts: any[] = [];
 
 		for (const postName of iterablePostFiles) {
-			const post = await import(postName);
-			console.log(postName);
-
+			// This calls import(path).
+			const post: any = await allPostFiles[postName]();
 			const slug = postName.split("/").pop()!.split(".").shift();
-			console.log(slug);
-
 			posts.push({ ...post.metadata, slug });
-			console.log("post push");
 		}
 
 		posts.sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
