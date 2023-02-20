290de13cf24b015c25e597c04512f69c5cee6dd0
a96lex <a96lex@gmail.com>
2023-02-20 16:06:45 +0100

    add code style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Footer from "$lib/components/Footer.svelte";
 	import Header from "$lib/components/Header.svelte";
 	import "$lib/styles/style.scss";
+	import "$lib/styles/code.scss";
 </script>
 
 <Header />
