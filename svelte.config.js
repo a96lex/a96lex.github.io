4271b05b101062c85fe1343f25670c7a3fa00823
a96lex <a96lex@gmail.com>
2023-02-20 18:20:55 +0100

    use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
-	
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
 	},
 
 	extensions: [".svelte", ".md"],
