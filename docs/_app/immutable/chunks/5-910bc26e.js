6b534e4e89267a9210892c616dfdda9b9255f84c
a96lex <a96lex@gmail.com>
2023-02-23 17:53:26 +0100

    update footer

--- docs/_app/immutable/chunks/5-910bc26e.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-188d044b.js";import{default as t}from"../components/pages/blog/_page.svelte-efc6d394.js";export{t as component,r as universal};
--- docs/_app/immutable/modules/pages/blog/_page.ts-5c078a78.js
+++
@@ -1,1 +0,0 @@
-import"../../../chunks/control-654b50f6.js";import"../../../chunks/index-cbbaa776.js";import{l as t}from"../../../chunks/_page-188d044b.js";export{t as load};
