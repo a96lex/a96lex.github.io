68b6ead5355e3d91e37a91264c0644b59340894f
a96lex <a96lex@gmail.com>
2025-01-04 19:35:19 +0100

    theme toggle, nice bg

---
+++ src/components/GradientBg.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let gradientStyle = "";
+
+  const updateGradient = (event: MouseEvent) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    gradientStyle = `background: radial-gradient(500px at ${x}px ${y}px, currentColor, transparent 70%)`;
+  };
+
+  onMount(() => {
+    window.addEventListener("mousemove", updateGradient);
+
+    return () => {
+      window.removeEventListener("mousemove", updateGradient);
+    };
+  });
+</script>
+
+<div
+  class="pointer-events-none fixed inset-0 z-30 transition-all duration-300 lg:absolute text-blue-300 dark:text-blue-700 opacity-10"
+  style={gradientStyle}
+></div>
