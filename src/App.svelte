b749652f2f4b42a777d4517020397205c5353381
a96lex <a96lex@gmail.com>
2025-01-07 17:59:06 +0100

    start to add some content

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
   import ThemeToggle from "./components/ThemeToggle.svelte";
   import GradientBg from "./components/GradientBg.svelte";
+  import About from "./components/About.svelte";
+  import Title from "./components/Title.svelte";
 </script>
 
 <main
   class="min-h-screen transition-all duration-500 bg-gray-200 dark:bg-gray-800"
 >
   <GradientBg />
-  <div class="flex justify-end items-center p-4">
+  <div class="flex justify-end items-center p-4 sticky">
     <ThemeToggle />
+  </div>
+
+  <div
+    class="flex flex-col items-start gap-10 max-w-4xl mx-auto"
+  >
+    <Title />
+    <About />
   </div>
 </main>
