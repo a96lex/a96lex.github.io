b6f200eace562ee797da5ffe6a71bca11c07c04c
a96lex <a96lex@gmail.com>
2025-01-07 17:59:34 +0100

    put in a div

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,8 +1,10 @@
-<h2 class="text-4xl font-bold py-2">Who am I</h2>
-<p class="text-2xl">
-  Results-driven Software Engineer with a proven track record of designing and
-  delivering scalable solutions for complex technical challenges. Expertise in
-  cloud-native development, optimizing machine learning pipelines, and creating
-  robust back-end systems. Dedicated to fostering innovation, team
-  collaboration, and technical excellence in fast-paced environments.
-</p>
+<div>
+  <h2 class="text-4xl font-bold py-2">Who am I</h2>
+  <p class="text-2xl">
+    Results-driven Software Engineer with a proven track record of designing and
+    delivering scalable solutions for complex technical challenges. Expertise in
+    cloud-native development, optimizing machine learning pipelines, and
+    creating robust back-end systems. Dedicated to fostering innovation, team
+    collaboration, and technical excellence in fast-paced environments.
+  </p>
+</div>
