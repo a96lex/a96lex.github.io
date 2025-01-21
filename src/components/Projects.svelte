6570c5945605d477cdff3b8aa97a1065f30cc0c3
a96lex <a96lex@gmail.com>
2025-01-21 03:07:21 +0100

    add more divs to fix image style

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -9,32 +9,34 @@
   <div class="flex flex-row flex-wrap justify-between">
     {#each projects as project}
       <div
-        class={`transition-all duration-300 mb-4 rounded-lg text-gray-800 dark:text-gray-200 w-full pb-2 lg:w-[calc(50%-0.5em)] relative `}
+        class={`transition-all duration-300 mb-4 rounded-lg text-gray-800 dark:text-gray-200 w-full pb-2 lg:w-[calc(50%-0.5em)] relative`}
       >
-        <h3
-          class={`text-lg font-bold bg-gray-300 dark:bg-gray-700 rounded-t-lg px-6 py-2 ${
-            project.image && "bg-opacity-50 dark:bg-opacity-50"
-          } backdrop-blur-sm`}
-        >
-          {project.name}
-        </h3>
         {#if project.image}
           <div
-            class="absolute rounded-lg inset-0 bg-gray-200 dark:bg-gray-800 opacity-50 bg-cover bg-center z-20"
+            class="absolute rounded-lg inset-0 bg-gray-200 dark:bg-gray-800 opacity-10 bg-cover bg-center pointer-events-none"
             style={`background-image: url('${project.image}');`}
           ></div>
         {/if}
-        <p class="px-6 mt-3 mb-6 z-30">{project.description}</p>
-        <div class="px-4 pb-2">
-          {#each project.tags as tag}
-            <span
-              class={`p-2 bg-gray-300 dark:bg-gray-700 m-1 rounded-md backdrop-blur-sm ${
-                project.image && "bg-opacity-50 dark:bg-opacity-50"
-              }`}
-            >
-              {tag}
-            </span>
-          {/each}
+        <div class="relative">
+          <h3
+            class={`text-lg font-bold bg-gray-300 dark:bg-gray-700 rounded-t-lg px-6 py-2 ${
+              project.image && "bg-opacity-50 dark:bg-opacity-50"
+            } backdrop-blur-sm`}
+          >
+            {project.name}
+          </h3>
+          <p class="px-6 mt-3 mb-6">{project.description}</p>
+          <div class="px-4 pb-2">
+            {#each project.tags as tag}
+              <span
+                class={`p-2 bg-gray-300 dark:bg-gray-700 m-1 rounded-md backdrop-blur-sm ${
+                  project.image && "bg-opacity-50 dark:bg-opacity-50"
+                }`}
+              >
+                {tag}
+              </span>
+            {/each}
+          </div>
         </div>
       </div>
     {/each}
