6ba3e55308026e643d94bc018468b333a0138e9e
a96lex <a96lex@gmail.com>
2025-01-21 07:28:25 +0100

    update copy

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,10 +1,13 @@
 <div>
   <h2>Who am I</h2>
   <p>
-    Results-driven Software Engineer with a proven track record of designing and
-    delivering scalable solutions for complex technical challenges. Expertise in
-    cloud-native development, optimizing machine learning pipelines, and
-    creating robust back-end systems. Dedicated to fostering innovation, team
-    collaboration, and technical excellence in fast-paced environments.
+    I'm a software engineer who loves solving problems and creating things that
+    people can actually use. From web apps to backend systems, I enjoy the
+    challenge of turning an idea into something functional. I'm always learning
+    and looking for new ways to improve. Whether I'm diving into new
+    technologies, optimizing workflows, or collaborating with others, I'm all
+    about making things better, faster, and more efficient. If you're looking
+    for someone who's passionate about tech and loves making a real impact,
+    that's me.
   </p>
 </div>
--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,4 +1,4 @@
 <div>
   <h1 class="text-5xl font-bold py-2">Alex Párraga Ferrer</h1>
-  <p>Creating stuff</p>
+  <p>Building things that work and make life a little easier</p>
 </div>
