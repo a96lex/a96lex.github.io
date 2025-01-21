05e3b295b1487ff41ca663b6b67d6344b4e6020d
a96lex <a96lex@gmail.com>
2025-01-21 08:01:06 +0100

    add contact section

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,4 +1,19 @@
 <div>
-  <h2>Contact me</h2>
-  <p>email</p>
+  <h2 class="text-2xl font-bold mb-4">Contact Me</h2>
+  <div class="flex space-x-6">
+    <a
+      href="https://www.linkedin.com/in/a96lex/"
+      target="_blank"
+      class="flex items-center space-x-2 hover:opacity-50 invert dark:invert-0"
+    >
+      <img src="linkedin.png" alt="LinkedIn" class="w-6 h-6" />
+    </a>
+    <a
+      href="mailto:a96lex@gmail.com"
+      target="_blank"
+      class="flex items-center space-x-2 hover:opacity-75 grayscale"
+    >
+      <img src="gmail.png" alt="Gmail" class="w-8 h-6" />
+    </a>
+  </div>
 </div>
